4a2e9e00353785f207552cc6b6c18530b0442251
dt1iusb <82310344+dt1iusb@users.noreply.github.com>
2021-04-11 04:42:56 -0400

    Add files via upload

---
+++ lab11.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#Dan Tran, Lab 11
+
+def words():
+
+    word1 = "Stapler before dawn"
+    word2 = "Waiting in perfect silence"
+    word3 = "Dreams of loose paper"
+
+    poem = word1 + "\n" + word2 + "\n" + word3
+    print poem
+
+if __name__ == '__main__':
+    words()
